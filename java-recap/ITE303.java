1f96e3ba6114d141b349e0184ef2af29e47d8277
Dr. Yad Tahir <yad@ieee.org>
2026-02-12 10:17:25 +0300

    java recap - part 2

---
+++ java-recap/ITE303.java
@@ -0,0 +1,48 @@
+public class ITE303 {
+	enum WEEKDAY {
+		SUNDAY,
+		MONDAY,
+		OTHER_DAY
+	}
+
+	void main() {
+		// var i = 0;
+		// int i = 0;
+		// while (i < 10 || true) {
+		// // if (i >= 10){
+
+		// // // continue;
+		// // // break;
+		// // // return;
+		// // }
+
+		// System.out.println("Hello AUIS " + i);
+		// i += 10_000;
+		// }
+
+		// int arr[] = new int[10];
+		// arr[0] = -2;
+		// arr[1] = -3;
+
+		// int arr2[] = { -2, -3, -4, -6, -10 };
+		// // int x1 = -2;
+		// // int x2 = -3;
+		// // int x3 = -4;
+		// for (int j = 0; j < 30; j++) {
+		// System.out.println(arr2[j % 5]);
+		// }
+
+		final int SUNDAY = 1;
+		final int MONDAY = 2;
+
+		attend(WEEKDAY.MONDAY);
+		// SUNDAY++;
+		// System.out.println(i);
+	}
+
+	public void attend(WEEKDAY day) {
+	}
+	// public void attend(int day){
+	// }
+
+}
